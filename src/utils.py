ee7ec3279cedf1157f4368c575bfcdada490a3a0
marijal74 <lakic.marija98@gmail.com>
2024-05-01 20:11:38 +0200

    Denormalize tensors when cuda is used

--- src/utils.py
+++ src/utils.py
@@ -6,7 +6,13 @@ import torch
 def print_sample(input, ground_truth, generated_img, row=0):
     input = input.detach().cpu()
     ground_truth = ground_truth.detach().cpu()
-    generated_img = generated_img[0].detach().cpu()
+    generated_img = generated_img.squeeze().detach().cpu()
+    # denormalize
+    if (torch.cuda.is_available()):
+        input = (input * 255).type(torch.uint8)
+        ground_truth = (ground_truth * 255).type(torch.uint8)
+        generated_img = (generated_img * 255).type(torch.uint8)
+
     imgs = [input, ground_truth, generated_img]
     _, axs = plt.subplots(ncols=3, squeeze=False)
     axs[0, 0].set_title('input')
