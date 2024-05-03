e70ccb449c2bf0a34c79d3d384cb043ca046c988
marijal74 <57067366+marijal74@users.noreply.github.com>
2024-05-03 18:07:33 +0200

    Fix discriminator final layer (#6)
    * Remove redundant sigmoid layer, as BCEWithLogitLoss is used

--- src/discriminator.py
+++ src/discriminator.py
@@ -6,21 +6,17 @@ import torch
 class Discriminator(nn.Module):
     def __init__(self, num_channels):
         super(Discriminator, self).__init__()
-        final = [
-            nn.Conv2d(512, 1, kernel_size=4, stride=1, padding=1),
-            nn.Sigmoid()
-            ]
         self.model = nn.Sequential(
             *Discriminator.__get_layer__(input=2*num_channels, out=64, batch_norm=False),
             *Discriminator.__get_layer__(input=64, out=128),
             *Discriminator.__get_layer__(input=128, out=256),
             *Discriminator.__get_layer__(input=256, out=512),
-            *final
+            nn.Conv2d(512, 1, 4, padding=1, bias=False)
         )
     @staticmethod
     def __get_layer__(input, out, batch_norm = True):
         layer = [
-            nn.Conv2d(in_channels=input, out_channels=out, kernel_size=4 , stride=2, padding=1)
+            nn.Conv2d(in_channels=input, out_channels=out, kernel_size=4 , stride=2, padding=1, bias=False)
         ]
         if batch_norm:
             layer.append(nn.BatchNorm2d(out))
