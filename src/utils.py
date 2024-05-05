fe1f355fee4be95e065d0a7192c08da52170392c
marijal74 <lakic.marija98@gmail.com>
2024-05-05 12:57:24 +0200

    Export utils as .py

--- src/utils.py
+++ src/utils.py
@@ -23,3 +23,19 @@ def print_sample(input, ground_truth, generated_img, row=0):
         axs[row, i].set_xticklabels([])
         axs[row, i].set_yticklabels([])
     plt.show()
+
+# %%
+def plot_loss(losses):
+  plt.plot(list(range(len(losses))), losses)
+  plt.xlabel('epoch')
+  plt.ylabel('loss')
+  plt.show()
+
+# %%
+def plot_losses(disc_loss, gen_loss):
+    plt.plot(list(range(len(disc_loss))), disc_loss, color='blue', label='discriminator loss')
+    plt.plot(list(range(len(gen_loss))), gen_loss, color='red', label = 'generator loss')
+    plt.legend(loc='upper left')
+    plt.xlabel('epoch')
+    plt.ylabel('loss')
+    plt.show()
