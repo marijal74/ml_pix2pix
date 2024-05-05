0d717d00a57deaeeeafc354528637514486038c0
marijal74 <57067366+marijal74@users.noreply.github.com>
2024-05-05 12:52:59 +0200

    Add missing droput layers in decoder (#7)
    * Add droput in decoder part

--- src/generator.py
+++ src/generator.py
@@ -51,9 +51,9 @@ class UNet(nn.Module):
         ]
     def __get_decoder__(self):
         return [
-            SkipConnection(UNet.up(input=512, out=512)),
-            SkipConnection(UNet.up(input=1024, out=512)),
-            SkipConnection(UNet.up(input=1024, out=512)),
+            SkipConnection(UNet.up(input=512, out=512, dropout=True)),
+            SkipConnection(UNet.up(input=1024, out=512, dropout=True)),
+            SkipConnection(UNet.up(input=1024, out=512, dropout=True)),
             SkipConnection(UNet.up(input=1024, out=512)),
             SkipConnection(UNet.up(input=1024, out=256)),
             SkipConnection(UNet.up(input=512, out=128)),
@@ -82,14 +82,14 @@ class UNet(nn.Module):
 
 
     def forward(self, x):
-        d1 = self.input(x) #(input=3, out=64,
-        d2 = self.down1(d1)  #(input=64, out=128)
-        d3 = self.down2(d2)  #(input=128, out=256)
-        d4 = self.down3(d3)  #(input=256, out=512)
-        d5 = self.down4(d4)  #(input=512, out=512)
-        d6 = self.down5(d5)  #(input=512, out=512)
-        d7 = self.down6(d6)  #(input=512, out=512)
-        d8 = self.down7(d7)  #(input=512, out=512)
+        d1 = self.input(x) 
+        d2 = self.down1(d1)
+        d3 = self.down2(d2)
+        d4 = self.down3(d3)
+        d5 = self.down4(d4)
+        d6 = self.down5(d5)
+        d7 = self.down6(d6)
+        d8 = self.down7(d7)
         
         u1 = self.up1(d8, d7)
         u2 = self.up2(u1, d6)
@@ -100,4 +100,3 @@ class UNet(nn.Module):
         u7 = self.up7(u6, d1)
         u8 = self.outermost(u7)
         return u8
-
