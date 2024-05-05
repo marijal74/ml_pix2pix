3edd065c63dd2ae109660cedd3f3f6249f1111c5
marijal74 <57067366+marijal74@users.noreply.github.com>
2024-05-05 15:22:12 +0200

    Small fixes (#10)
    * Use L1 loss
    
    * Losses small refactor

--- src/losses.py
+++ src/losses.py
@@ -10,22 +10,19 @@ loss_comparison = nn.BCEWithLogitsLoss()
 L1_loss = nn.L1Loss()
 
 # %%
-def train_discriminator_step(discriminator: Discriminator, generator:UNet, inputs, targets, opt, device):
+def train_discriminator_step(discriminator: Discriminator, generator:UNet, inputs, targets, opt):
     opt.zero_grad()
 
     # real image loss
     output = discriminator(inputs, targets)
-    label = torch.ones(size = output.shape, dtype=torch.float, device=device)
-        
-    real_loss = loss_comparison(output, label)
+    real_loss = loss_comparison(output, True)
 
     gen_image = generator(inputs).detach()
 
     # fake image loss
     fake_output = discriminator(inputs, gen_image)
-    fake_label = torch.zeros(size = fake_output.shape, dtype=torch.float, device=device) 
     
-    fake_loss = loss_comparison(fake_output, fake_label)
+    fake_loss = loss_comparison(fake_output, False)
 
     total_loss = (real_loss + fake_loss)/2
 
@@ -36,16 +33,15 @@ def train_discriminator_step(discriminator: Discriminator, generator:UNet, input
     return total_loss
 
 # %%
-def generator_training_step(discriminator: Discriminator, generator:UNet, inputs, targets, opt, device, L1_lambda = 100):
+def generator_training_step(discriminator: Discriminator, generator:UNet, inputs, targets, opt, L1_lambda = 100):
           
     opt.zero_grad()
     
     generated_image = generator(inputs)
     
     disc_output = discriminator(inputs, generated_image)
-    desired_output = torch.ones(size = disc_output.shape, dtype=torch.float, device=device)
     
-    generator_loss = loss_comparison(disc_output, desired_output) + L1_lambda * L1_loss(generated_image, targets)
+    generator_loss = loss_comparison(disc_output, True) + L1_lambda * L1_loss(generated_image, targets)
     generator_loss.backward()
     opt.step()
 
